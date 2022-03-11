9b14e48d6f48c51c7aa84bf4e167690ba7ff6c02
IsabelleOrmo <bellecrenonini@gmail.com>
2022-03-11 07:46:23 -0300

    Main do programa do retângulo pronta

--- 2022-03-08/CalculadorRetângulo/src/App.java
+++ 2022-03-08/CalculadorRetângulo/src/App.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insira os valores do retângulo:  ");
+        Retangulo ret1 = new Retangulo(input.nextInt(), input.nextInt());
 
+        System.out.println("Perímetro:  " + ret1.perimetro());
+        System.out.println("Área:  " + ret1.area());
+
+        input.close();
     }
 }
