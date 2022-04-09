3ba7942286fe0b33e4863b92fbe7371549ddc9b6
ame-meida <ame.pistache@gmail.com>
2022-04-09 19:57:11 -0300

    Alterações irrelevantes

--- 2022-04-05/Banco/src/AppContas.java
+++ 2022-04-05/Banco/src/AppContas.java
@@ -4,7 +4,6 @@ public class AppContas {
         ContaPoupanca contapoup1 = new ContaPoupanca("Jeremias", 1000);
         ContaEspecial contaesp1 = new ContaEspecial("Michel", 1500, 500);
         
-        
         contabanc1.testaConta();
         contapoup1.testaConta();
         contaesp1.testaConta();
