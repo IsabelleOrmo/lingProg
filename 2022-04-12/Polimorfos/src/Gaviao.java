2ee71fc68fdb7aa18bfe7ca3b44a8ce91a6717c1
IsabelleOrmo <bellecrenonini@gmail.com>
2022-04-12 17:05:27 -0300

    Mais polimorfismo

---
+++ 2022-04-12/Polimorfos/src/Gaviao.java
@@ -0,0 +1,10 @@
+public class Gaviao extends Object {
+    @Override
+    public void falar() {
+        System.out.println("* grasna *");
+    }
+
+    public void cacar() {
+        System.out.println("Você caçou um filhote de hamster!");
+    }
+}
