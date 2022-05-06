6d05376cc6b0b5077d438fe73124605579f1f674
IsabelleOrmo <bellecrenonini@gmail.com>
2022-05-06 07:55:00 -0300

    Copiados os arquivos de Imóveis

--- 2022-05-06/Iterator/src/App.java
+++ 2022-05-06/Iterator/src/App.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Imoveis> listaImoveis = new ArrayList<Imoveis>();
+        
+        Novo imovel1 = new Novo("Avenida da Tristeza", 700000, 100); 
+        Novo imovel2 = new Novo("Travessa da Amargura", 40000, 9000);
+        Antigo usado1 = new Antigo("Alameda da Angústia", 82000, 1000); 
+        Antigo usado2 = new Antigo("Entrada do Desespero", 45000, 200); 
+
+        listaImoveis.add(imovel1); 
+        listaImoveis.add(imovel2);
+        listaImoveis.add(usado1);
+        listaImoveis.add(usado2);
+        listaImoveis.add(new Novo("Rotatória das Decepções", 23000, 2000));
+
+
+        // Exemplo de uso de Iterators, com a booleana "hasNext() e a função remove"
+        Iterator<Imoveis> it = listaImoveis.iterator();
+
+        while (it.hasNext()) {
+            if (it.next().getValor() < 50000) {
+                it.remove();
+            }
+        }
+
+        System.out.print("Lista de Imóveis NOVOS disponíveis:\n\n");
+        Imoveis.showNovo(listaImoveis);
     }
 }
---
+++ 2022-05-06/Iterator/src/Imoveis.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public abstract class Imoveis {
+    private String endereco;
+    private float valor;
+    private static int qntImoveis = 0;
+    private int codImovel;
+
+
+    // Construtores
+    public Imoveis() {
+        endereco = "";
+        valor = 0;
+        codImovel = ++qntImoveis;
+    }
+
+    public Imoveis(String endereco, float valor) {
+        this.endereco = endereco; 
+        this.valor = valor;
+        codImovel = ++qntImoveis;
+    }
+
+
+    // Métodos
+    public void imprimirValores() {
+        System.out.println("Código do imóvel:  " + getCodImovel());
+        System.out.printf("Valor do imóvel:  R$ %.2f\n", getValor());
+        System.out.println("Endereço:  " + getEndereco());
+    }
+
+    public static void showNovo(ArrayList<Imoveis> listaImoveis) {
+        for (Imoveis imovel : listaImoveis) {
+            if (imovel instanceof Novo) {
+                Novo imovelNovo = (Novo) imovel;
+                imovelNovo.imprimirValores();
+            }
+        }
+    }
+
+    public static void showVelho(ArrayList<Imoveis> listaImoveis) {
+        for (Imoveis imovel : listaImoveis) {
+            if (imovel instanceof Antigo) {
+                Antigo usadoImovel = (Antigo) imovel;
+                usadoImovel.imprimirValores();
+            }
+        }
+    }
+
+    public static void showAll(ArrayList<Imoveis> listaImoveis) {
+        for (Imoveis imovel : listaImoveis) {
+            if (imovel instanceof Novo) {
+                Novo imovelNovo = (Novo) imovel;
+                imovelNovo.imprimirValores();
+            } 
+
+            if (imovel instanceof Antigo) {
+                Antigo usadoImovel = (Antigo) imovel;
+                usadoImovel.imprimirValores();
+            }   
+        }
+    }
+
+
+    // Getters e setters
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public float getValor() {
+        return this.valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public int getCodImovel() {
+        return codImovel;
+    }
+
+    public static int getQntImoveis() {
+        return qntImoveis;
+    }
+}
