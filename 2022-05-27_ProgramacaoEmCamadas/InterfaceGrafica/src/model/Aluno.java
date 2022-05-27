284766a41610ec9038aa62861e02e049352b563c
IsabelleOrmo <bellecrenonini@gmail.com>
2022-05-27 09:04:00 -0300

    Projeto inútil no qual eu nunca vou encostar

---
+++ 2022-05-27_ProgramacaoEmCamadas/InterfaceGrafica/src/model/Aluno.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Aluno {
+    // Atributos da classe
+    private String nome;
+    private int idade;
+
+
+    // Métodos da classe
+    public Aluno(String nome, int idade) {
+        setNome(nome);
+        setIdade(idade);
+    }
+    
+
+    // Getters e setters
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+}
