68060d1c65036e0180144971267147e8200ceace
AmeMeida <ame.pistache@gmail.com>
2022-08-12 09:08:36 -0300

    Tarefa: MVC de produto

---
+++ 2022-08-12_JTable/MVC_Produto/src/control/ProdutoControl.java
@@ -0,0 +1,37 @@
+package control;
+
+import java.security.cert.PKIXRevocationChecker.Option;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import model.Produto;
+
+public class ProdutoControl {
+    private ArrayList<Produto> lstProdutos;
+
+    public ProdutoControl() {
+        this.lstProdutos = new ArrayList<Produto>();
+    }
+
+    public void cadastrar(String codigo, String descricao, double preco) {
+        lstProdutos.add(new Produto(codigo, descricao, preco));
+    }
+
+    public Optional<Produto> buscaCod(String codigo) {
+        for (Produto produto : lstProdutos) {
+            if (produto.getCodigo().equals(codigo))
+                return Optional.of(produto);
+        }
+
+        Optional.empty();
+    }
+
+    public Optional<Produto> buscaDesc(String descricao) {
+        for (Produto produto : lstProdutos) {
+            if (produto.getDescricao().startsWith(descricao))
+                return Optional.of(produto);
+        }
+
+        Optional.empty();
+    }
+}
---
+++ 2022-08-12_JTable/MVC_Produto/src/model/Produto.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author aluno
+ */
+public class Produto {
+    private String codigo;
+    private String descricao;
+    private double preco;
+
+
+    public Produto(String codigo, String descricao, double preco) {
+        setCodigo(codigo);
+        setDescricao(descricao);
+        setPreco(preco);
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+
+    
+	private void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	private void setDescricao(String descricao) {
+		this.descricao = descricao;
+    }
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+}
